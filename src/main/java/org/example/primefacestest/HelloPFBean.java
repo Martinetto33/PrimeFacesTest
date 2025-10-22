2d7a3e7c114aa479bb0805e7fd2935df47d7fc2f
Alin Bordeianu <martynnn33@gmail.com>
2025-10-22 14:49:28 +0200

    feat: added Primefaces datatable

--- src/main/java/org/example/primefacestest/HelloPFBean.java
+++ src/main/java/org/example/primefacestest/HelloPFBean.java
@@ -1,7 +1,52 @@
 package org.example.primefacestest;
 
+import jakarta.enterprise.context.SessionScoped;
 import jakarta.inject.Named;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
 @Named("helloPFBean")
-public class HelloPFBean {
+@SessionScoped
+public class HelloPFBean implements Serializable {
+    private String firstName = "Prime";
+    private String lastName = "Faces";
+    private String componentSuite;
+    private List<Technology> technologies = List.of(
+            new Technology("Java", "10"),
+            new Technology("Spring", "5.0")
+    );
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getComponentSuite() {
+        return componentSuite;
+    }
+
+    public void setComponentSuite(String componentSuite) {
+        this.componentSuite = componentSuite;
+    }
+
+    public List<Technology> getTechnologies() {
+        return technologies;
+    }
+
+    public void setTechnologies(List<Technology> technologies) {
+        this.technologies = technologies;
+    }
 }
